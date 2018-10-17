0a1859f06568dfa97699547d202961fed082db3b
Vishal Mistry <vishalmistry51@gmail.com>
2018-10-17 14:20:07 +0100

    added revenue per iteration

--- components/ventures/assets.js
+++ components/ventures/assets.js
@@ -2,33 +2,41 @@ export default {
     OJS: {
         name: 'Orange Juice Stand',
         image: require('../static/orange-juice.png'),
+        Ri: 1,
     },
     NPS: {
         name: 'Newspaper Stand',
         image: require('../static/newspaper.jpg'),
+        Ri: 5,
     },
     SHP: {
         name: 'Shop',
         image: require('../static/shop.png'),
+        Ri: 50,
     },
     PHN: {
         name: 'Phone Company',
         image: require('../static/phone.png'),
+        Ri: 1000,
     },
     MEC: {
         name: 'Mechanics',
         image: require('../static/mechanic.png'),
+        Ri: 300,
     },
     HTC: {
         name: 'Hotel Business',
         image: require('../static/hotel-business.png'),
+        Ri: 10000,
     },
     FLM: {
         name: 'Film Studio',
         image: require('../static/film.png'),
+        Ri: 100000,
     },
     ROK: {
         name: 'Rocket Company',
         image: require('../static/rocket.png'),
+        Ri: 100000,
     }
 }
