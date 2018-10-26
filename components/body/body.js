61f6ffe16b3053f5efc059f28bc49528b4fe90a0
Vishal Mistry <vishalmistry51@gmail.com>
2018-10-26 02:42:39 +0100

    added rest of ventures

--- components/body/body.js
+++ components/body/body.js
@@ -8,18 +8,20 @@ import Assets from '../ventures/assets';
 // WU: what is the point of the test data
 // Add individual images
 
-const money = 1000000000;
-
 const body = () => {
     return (
         <View style={styles.itemsContainer}>
             <View style={styles.bankContainer}>
-                <Text style={styles.bank}>${money}</Text>
+                <Text style={styles.bank}>${Assets.Money}</Text>
             </View>
             <ScrollView>
                 <Venture asset={Assets.OJS} />
                 <Venture asset={Assets.NPS} />
+                <Venture asset={Assets.SHP} />
                 <Venture asset={Assets.MEC} />
+                <Venture asset={Assets.HTC} />
+                <Venture asset={Assets.FLM} />
+                <Venture asset={Assets.ROK} />
             </ScrollView>
         </View>
     );
