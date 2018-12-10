d0254dad4158c7c70d8977b6da1737a6073c851b
Vishal Mistry <vishalmistry51@gmail.com>
2018-12-10 11:34:47 +0000

    cleared body styles of redundant code

--- components/body/bodyStyles.js
+++ components/body/bodyStyles.js
@@ -1,14 +1,6 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    // Main Body
-    body: {// this isnt used
-        alignItems: 'center',
-        alignContent: 'center',
-        justifyContent: 'center',
-        marginVertical: 20,
-        backgroundColor: '#FFD54F',
-    },
     // Bank
     bankText: {
         justifyContent: 'center',
@@ -27,44 +19,4 @@ export default StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'stretch',
     },
-    // Unused styles... till end
-    itemHeader: {
-        color: 'black',
-        fontSize: 18,
-        marginBottom: 8,
-        alignSelf: 'center',
-        color: '#fff'
-    },
-    
-    item: {
-        backgroundColor: '#FFC107',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignSelf: 'stretch',
-        marginVertical: 10,
-        paddingVertical: 10,
-    },
-    
-    container1: {
-        flex: 1,
-        marginHorizontal: 10,
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
-    },
-    container2: {
-        marginHorizontal: 10,
-        flexDirection: 'column',
-        flex: 1
-    },
-    image: {
-        height: 80,
-        width: 80,
-        marginHorizontal: 10,
-        resizeMode: 'contain',
-        borderRadius: 10,
-    },
-    // Progress Bar
-    progressBar: {
-        paddingVertical: 10,
-    }
 });
